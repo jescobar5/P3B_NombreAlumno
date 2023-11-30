f2589ed5d0640e1db7c3a035c8c8ececcd235e4e
jescobar5 <151049552+jescobar5@users.noreply.github.com>
2023-11-30 17:21:32 +0100

    Update imprimeNombre.java

--- Proyecto3B/src/imprimeNombre.java
+++ Proyecto3B/src/imprimeNombre.java
@@ -3,7 +3,7 @@ public class imprimeNombre {
 
 	public static void main(String[] args) {
 
-		System.out.println("Juan Luis Escobar López");
+		System.out.println("Estoy llegando al final");
 	}
 
 }
